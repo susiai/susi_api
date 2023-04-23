13f3478c45a56244fc573e375ec9a3cfbaf5b191
Michael Peter Christen <mc@yacy.net>
2023-04-23 17:39:44 +0200

    added vosk (stub)

---
+++ data/model/README_model.md
@@ -0,0 +1,4 @@
+# Model location
+
+This is the place where you should put a model, i.e.
+- Vosk models downloaded from https://alphacephei.com/vosk/models
--- src/audio/audio_service.py
+++ src/audio/audio_service.py
@@ -1,8 +1,10 @@
 import io, requests, tempfile
+import wave
 from flask import jsonify, request, current_app
 from flask_restx import Namespace, Resource
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import whisper
+from vosk import Model, KaldiRecognizer, SetLogLevel
 
 api = Namespace('api/audio', description='text operations')
 
@@ -35,21 +37,53 @@ class VoiceTranscription(Resource):
             response = requests.post("https://api.openai.com/v1/audio/transcriptions", headers=headers, data=payload)
             result = response.json()
         else:
+            # make an offline transcription
+
             # patch model name in case no api key is present
             if model_name == 'whisper-1': model_name = 'tiny'
 
-            # Load the offline model
-            model = whisper.load_model(model_name)
+            # check the required technology (whisper or vosk)
+            if model_name == 'tiny' or model_name == 'base' or model_name == 'small' or model_name == 'medium' or model_name == 'large': 
 
-            # Save the received audio file temporarily to disk
-            with tempfile.NamedTemporaryFile(suffix=".wav", delete=True) as temp_audio_file:
-                temp_audio_file.write(audio_data)
-                temp_audio_file.flush()
+                # Load the offline model
+                model = whisper.load_model(model_name)
 
-                # Transcribe using the offline model
-                result = model.transcribe(temp_audio_file.name)
+                # Save the received audio file temporarily to disk
+                with tempfile.NamedTemporaryFile(suffix=".wav", delete=True) as temp_audio_file:
+                    temp_audio_file.write(audio_data)
+                    temp_audio_file.flush()
 
-            result = {'text': result['text']}
-            
+                    # Transcribe using the offline model
+                    result = model.transcribe(temp_audio_file.name)
+
+                result = {'text': result['text']}
+
+            if model_name == 'en-us' or model_name == 'de': 
+                # use vosk transcriptions
+
+                wf = wave.open(io.BytesIO(audio_data), "rb")
+                if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+                    return jsonify({'error': 'Audio file must be WAV format mono PCM.'})
+
+                model = Model('en-us')
+                rec = KaldiRecognizer(model, wf.getframerate())
+                rec.SetWords(True)
+                rec.SetPartialWords(True)
+                
+                transcript = ""
+
+                while True:
+                    data = wf.readframes(4000)
+                    if len(data) == 0:
+                        break
+                    if rec.AcceptWaveform(data):
+                        result = rec.Result()
+                        transcript += result["text"]
+                    else:
+                        partial_result = rec.PartialResult()
+                        if partial_result:
+                            transcript += partial_result["partial"]
+
+                result = {'text': transcript}
+                    
         return result
-
